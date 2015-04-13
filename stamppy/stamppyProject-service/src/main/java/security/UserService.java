d6cb9edb39d22ef5b3e52aae535e3d5f8e4c790a
Ana Barragan <anamicharry@hotmail.com>
2015-04-13 17:55:18 -0500

    Se agregan operaciones de actualizar, traer y eliminar usuario

--- stamppy/stamppyProject-service/src/main/java/security/UserService.java
+++ stamppy/stamppyProject-service/src/main/java/security/UserService.java
@@ -3,13 +3,20 @@ package security;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.stamppyProject.businessLogic.security.IUser;
 import org.stamppyProject.businessLogic.security.dto.UserJson;
+
+import business.StampJson;
 
 
 @Stateless
@@ -23,10 +30,30 @@ public class UserService {
      @POST
      @Path("/register")
      @Consumes(MediaType.APPLICATION_JSON)
-     public Response registerUser(UserJson userJson)
-     {
-    	userBean.registerUser(userJson);;
+     public Response registerUser(UserJson userJson){
+    	userBean.registerUser(userJson);
         return Response.status(200).build();
      }
+     
+ 	@PUT
+ 	@Path("/user")
+ 	@Consumes(MediaType.APPLICATION_JSON)
+ 	public Response updateUser(UserJson userJson){
+ 		userBean.updateUser(userJson); 		
+ 		return Response.status(200).build();
+ 	}
+ 	
+ 	@GET
+	@Path("/user/{id}")
+	@Produces("application/json")
+	public UserJson getStamp(@PathParam("id") Long id){
+		return userBean.getSUser(id);
+	}
+ 	
+ 	@DELETE
+	@Path("/user/{id}")
+	public Response removeUser(@PathParam("id") Long id){
+ 		return Response.status(200).build();
+ 	}
      
 }
