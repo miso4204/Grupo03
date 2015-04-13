e08b759b123a72a674627a84ed50d5a9f6392d26
Ana Barragan <anamicharry@hotmail.com>
2015-04-13 17:31:49 -0500

    Se agregan los métodos del Bean

--- stamppy/stamppyProject-businessLogic/src/main/java/org/stamppyProject/businessLogic/security/IUser.java
+++ stamppy/stamppyProject-businessLogic/src/main/java/org/stamppyProject/businessLogic/security/IUser.java
@@ -6,5 +6,9 @@ import org.stamppyProject.businessLogic.security.dto.UserJson;
 public interface IUser {
 	
 	public void registerUser(UserJson userJson);
+	
+	public void updateUser(UserJson userJson);
+	
+	public UserJson getSUser(Long id);
 
 }
