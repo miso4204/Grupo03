90fa5265356cefeaf1ea9db8187d20821b64d055
anfosoriova <anfosoriova@gmail.com>
2015-04-26 15:36:40 -0500

    Report and Cart fix, integration with Rest Service implemented

---
+++ web_app/client/app/reportService/reportService.service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('webAppApp')
+  .factory('ReportSalesId', 
+  	function ($resource) {
+      var resource = $resource('http://uniandes-msls.rhcloud.com/salesReport-service/rest/salesReport-service/sales/:id', {id:'@id'});
+      return resource;      
+    }
+  	)
+  .factory('ReportSalesPeriod', 
+  	function ($resource) {
+      var resource = $resource('http://uniandes-msls.rhcloud.com/salesReport-service/rest/salesReport-service/sales/:limiteBajo/:limiteAlto', {limiteBajo:'@limiteBajo', limiteAlto:'@limiteAlto'});
+      return resource;      
+    }
+  	)
+  .factory('ReportRatingId', 
+    function ($resource) {
+      var resource = $resource('http://uniandes-msls.rhcloud.com/ratingReport-service/rest/ratingReport-service/rating/:artistaId', {artistaId:'@artistaId'});
+      return resource;      
+    }
+    )
+  .factory('ReportRatings', 
+    function ($resource) {
+      var resource = $resource('http://uniandes-msls.rhcloud.com/ratingReport-service/rest/ratingReport-service/rating');
+      return resource;      
+    }
+    );
