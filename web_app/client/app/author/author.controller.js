4d31233a25f2fc866e2fd0f4da25a5669f8225fe
Ana <anamicharry@gmail.com>
2015-04-26 15:42:17 -0500

    chnges author

--- web_app/client/app/author/author.controller.js
+++ web_app/client/app/author/author.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('webAppApp')
   .controller('AuthorCtrl', function ($scope) {
-   	$scope.myInterval=500;
+   	$scope.myInterval=1000;
    	$scope.slides=[
    		{url:"http://i.imgur.com/0w2Jx7Cm.png",text:"hola"},
    		{url:"http://i.imgur.com/uKwBXQnm.jpg",text:"hola1"},
