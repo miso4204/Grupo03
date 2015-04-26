894eb6183cca1af0de7279ab4499cee6bc2dad4a
julian <jcvgalfonso@gmail.com>
2015-04-26 14:39:13 -0500

    Author gallery changed
    Author gallery changed

--- web_app/client/app/author/author.controller.js
+++ web_app/client/app/author/author.controller.js
@@ -2,5 +2,12 @@
 
 angular.module('webAppApp')
   .controller('AuthorCtrl', function ($scope) {
-    
+   	$scope.myInterval=500;
+   	$scope.slides=[
+   		{url:"http://i.imgur.com/0w2Jx7Cm.png",text:"hola"},
+   		{url:"http://i.imgur.com/uKwBXQnm.jpg",text:"hola1"},
+   		{url:"http://i.imgur.com/0w2Jx7Cm.png",text:"hola2"},
+   		{url:"http://i.imgur.com/uKwBXQnm.jpg",text:"hola3"},
+   		{url:"http://i.imgur.com/0w2Jx7Cm.png",text:"hola4"},
+   	]
   });
