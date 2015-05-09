ea210aaf4d0ec327425de40dea113d3f27d396b7
cemartinezca <cemartinezca@gmail.com>
2015-05-09 18:36:30 -0500

    Forma de actualización de datos de usuario
    Creación de la forma de actualización de datos de usuario que reemplaza
    a la forma de cambio de contraseña.

--- web_app/client/app/change-user-data/change-user-data.controller.js
+++ web_app/client/app/change-user-data/change-user-data.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('webAppApp')
-  .controller('ChangePasswordCtrl', function ($scope,$routeParams,$rootScope,User,UpdateUser, sessionStorage) {
+  .controller('ChangeUserDataCtrl', function ($scope,$routeParams,$rootScope,User,UpdateUser, sessionStorage) {
     if (sessionStorage.get("user")) {
       if(!$rootScope.globals){
         $rootScope.globals={};
@@ -14,17 +14,44 @@ angular.module('webAppApp')
     $scope.oldPassword = "";
     $scope.newPassword = "";
     $scope.newPasswordConfirm = "";
+    $scope.name = "";
+    $scope.lastName = "";
+    $scope.email = "";
     
     $scope.dataUser = User.get({id:$rootScope.globals.currentUser.userId}, 
       function(){
         console.log(JSON.stringify($scope.dataUser))
       });
     
-    $scope.update = function () {
+    $scope.updateUser = function () {
+      console.log("Actualizando Información de usuario...");
+      //console.log($scope.dataUser)
+      $scope.regSuccessUsrShow = false;
+      $scope.regErrorUsrShow = false;
+      //console.log("Contraseña Anterior:"+$scope.dataUser.password);
+      //console.log("Contraseña Digitada:"+$scope.oldPassword);
+      //console.log("Contraseña Nueva:"+$scope.newPassword);
+      //console.log("Confirmaciòn:"+$scope.newPasswordConfirm);
+      
+      $scope.dataUser.name = $scope.name;
+      $scope.dataUser.lastName = $scope.lastName;
+      $scope.dataUser.email = $scope.email;
+      //console.log(JSON.stringify($scope.dataUser));
+      status = UpdateUser.update($scope.dataUser);
+      //console.log("Register responsed" + JSON.stringify(response))
+      $scope.regSuccess= "Actualizaciòn de datos exitosa";
+      $scope.regSuccessUsrShow = true;
+      $scope.regErrorUsrShow = false;
+      $scope.name = "";
+      $scope.lastName = "";
+      $scope.email = "";
+    }
+    
+    $scope.updatePassword = function () {
       console.log("Actualizando Contraseña...");
       console.log($scope.dataUser)
-      $scope.regSuccessShow = false;
-      $scope.regErrorShow = false;
+      $scope.regSuccessPwdShow = false;
+      $scope.regErrorPwdShow = false;
       //console.log("Contraseña Anterior:"+$scope.dataUser.password);
       //console.log("Contraseña Digitada:"+$scope.oldPassword);
       //console.log("Contraseña Nueva:"+$scope.newPassword);
@@ -38,28 +65,28 @@ angular.module('webAppApp')
             status = UpdateUser.update($scope.dataUser);
             //console.log("Register responsed" + JSON.stringify(response))
             $scope.regSuccess= "Cambio de contraseña exitosa";
-            $scope.regSuccessShow = true;
-            $scope.regErrorShow = false;
+            $scope.regSuccessPwdShow = true;
+            $scope.regErrorPwdShow = false;
             $scope.oldPassword = "";
             $scope.newPassword = "";
             $scope.newPasswordConfirm = "";
           } else {
             $scope.regError= "Las contraseñas no coinciden";
             console.log("error");
-            $scope.regSuccessShow = false;
-            $scope.regErrorShow = true;    
+            $scope.regSuccessPwdShow = false;
+            $scope.regErrorPwdShow = true;    
           }
         } else {
           $scope.regError= "La contraseña debe tener más de 6 caracteres";
           console.log("error");
-          $scope.regSuccessShow = false;
-          $scope.regErrorShow = true;
+          $scope.regSuccessPwdShow = false;
+          $scope.regErrorPwdShow = true;
         }
       } else {
         $scope.regError= "La contraseña digitada no coincide con la actual";
         console.log("error");
-        $scope.regSuccessShow = false;
-        $scope.regErrorShow = true;
+        $scope.regSuccessPwdShow = false;
+        $scope.regErrorPwdShow = true;
       }
     }
   });
