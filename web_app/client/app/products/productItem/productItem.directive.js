db2aff5e9d18b32177fa101a0248df7922eb524b
anfosoriova <anfosoriova@gmail.com>
2015-05-10 19:45:42 -0500

    Cart fix

--- web_app/client/app/products/productItem/productItem.directive.js
+++ web_app/client/app/products/productItem/productItem.directive.js
@@ -2,7 +2,7 @@
 
 angular.module('webAppApp')
   .directive('productItem', function () {
-    var controller=['$rootScope','CartP', '$scope','sessionStorage', function($rootScope,CartP,$scope,sessionStorage){
+    var controller=['$rootScope','CartP', '$scope','sessionStorage', 'Cart', function($rootScope,CartP,$scope,sessionStorage,Cart){
       
       $scope.addToCart=function(product){
         console.log(product)
@@ -15,6 +15,18 @@ angular.module('webAppApp')
         prod.sta
         CartP.save(product,function(response){
           console.log(response);
+          var result = {};
+          result= Cart.get({id:$rootScope.globals.currentUser.userId},
+              function(){
+                        sessionStorage.remove("products");
+                        $rootScope.products=result.products;
+                        console.log($rootScope.products);
+                        sessionStorage.set("products",$rootScope.products);
+                        $scope.idCart = result.id;
+                        $rootScope.nroProdCart = $rootScope.products.length;
+                        console.log($rootScope.nroProdCart)
+              }
+          );
           $location.path('/products');
         })
       }
